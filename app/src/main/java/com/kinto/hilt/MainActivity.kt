655c396aff40111d48652c9ea1fc1a3f25f2dd53
hyunjong.kim <hyunjong.kim@kinto-technologies.com>
2022-10-14 09:30:04 +0900

    first injection     implementation 'androidx.activity:activity-compose:1.3.1' ->    implementation 'androidx.activity:activity-compose:1.6.0' not updated error occur

--- app/src/main/java/com/kinto/hilt/MainActivity.kt
+++ app/src/main/java/com/kinto/hilt/MainActivity.kt
@@ -11,8 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kinto.hilt.ui.theme.HiltComposeUITheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var car: Car
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(car.modelCompany())
                 }
             }
         }
